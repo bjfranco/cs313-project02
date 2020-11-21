ff56a20c5d5e2299e7f3e8de9fdf34881113f801
bjfranco <fra18020@byui.edu>
2020-11-21 12:56:31 -0700

    Testing API

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const api_key = '5dd7d13462ea2b7ba5176cd85add7658';
 
+function getWeather(){
+	var city = document.getElementById("city").value;
+	console.log("Beggining weather search in " + city);
+
+	var url = 'api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5dd7d13462ea2b7ba5176cd85add7658'
+
+	fetch(url)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(data) {
+		updatePage(data);
+	})
+}
+
+function updatePage(data){
+	console.log(data);
+	document.getElementById('response').innerHTML = data;
+}
 
 /*
 app.post('/weather', (req, res) => {
@@ -46,11 +65,12 @@ function setValue(temp){
 
 
 
-app.get('/weather', (req, res) => {
+/*app.get('/weather', (req, res) => {
 	const api_url = 'api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5dd7d13462ea2b7ba5176cd85add7658';
 	const response = fetch(api_url);
 	const json = response.json();
 	console.log(json);
 	//res.json(json);
 });
+*/
 
