e4b3c4da84132c0eadda09fc3f5bc1e213af1283
bjfranco <fra18020@byui.edu>
2020-11-21 18:10:17 -0700

    Adding Styles

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public/stylesheets')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'))
